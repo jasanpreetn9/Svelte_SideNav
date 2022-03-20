e2a28efa7c1732cff7198f31a899a021591b39a2
Jasanpreet Nagra <57871055+jasanpreetn9@users.noreply.github.com>
2022-03-19 19:23:11 -0500

    Create Index.svelte

---
+++ Index.svelte
@@ -0,0 +1,26 @@
+<script>
+	import globals from '../global.css';
+	import Sidebar from '../components/Nav.svelte';
+	let sidebar_show = false;
+</script>
+
+<scelte:head>
+	<title>Anime</title>
+</scelte:head>
+<button on:click={() => (sidebar_show = !sidebar_show)}>&#9776</button>
+
+
+<Sidebar bind:show={sidebar_show} />
+<slot />
+
+<style>
+	button {
+		left: 6;
+		top: 15px;
+		border: none;
+		color: white;
+		background-color: transparent;
+		font-size: 40px;
+		cursor: pointer;
+	}
+</style>
